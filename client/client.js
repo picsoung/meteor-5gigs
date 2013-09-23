1d7437e0af6779d17d9505528a1f516771d64772
Nicolas Grenié <nicolas.grenie@gmail.com>
2013-09-23 13:24:11 +0200

    add map and country

--- client/client.js
+++ client/client.js
@@ -1,13 +1,64 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to 5gigmap.";
-  };
+  L.Icon.Default.imagePath = 'packages/leaflet/images'
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.searchForm.events({
+    'click #searchButton' : function (event) {
+      Meteor.call('search5gig', $("#cityName").val(),$("#country").val(),function(err,data){
+        if(err)
+          return err;
+
+        data.forEach(function (concert) {
+          var marker = L.marker([concert.venue.location.latitude, concert.venue.location.longitude]).addTo(map);
+          marker.bindPopup("<b><a href='"+concert.url+"'>"+concert.name+"</a></b><br><b>Date </b>"+concert.startDate);
+        });
+
+        Session.set('city', $("#cityName").val());
+        Session.set('concerts',data);
+      });
+
+      Meteor.call('getGeoCoordinates',$("#cityName").val(),function(err,data){
+        if(err)
+          return err
+
+        Session.set('coordinates',data);
+        map.setView(new L.LatLng(data.lat, data.lon), 13) 
+      });
+
+      event.preventDefault();
     }
   });
+
+  Template.concert.events({
+    'click #seemap' : function (event){
+      var concert = this;
+      map.setView(new L.LatLng(concert.venue.location.latitude, concert.venue.location.longitude), 20)
+    }
+  });
+
+
+  Template.maptemplate.rendered = function () {
+    var lat = Session.get('coordinates') ? Session.get('coordinates').lat : 47;
+    var lon = Session.get('coordinates') ? Session.get('coordinates').lon : 20;
+    var zoom = 5;
+    window.map = L.map('map').setView([lat, lon], zoom);
+    L.tileLayer('http://{s}.tile.cloudmade.com/139c31bba7f14a5bba50e9ae3f9802fc/997/256/{z}/{x}/{y}.png?token=96f4ae8a17f5435ca8adcc322e29058d', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+        maxZoom: 18
+    }).addTo(window.map);
+  };
+
+  Template.eventsMap.helpers({
+    concerts: function () {
+      return Session.get('concerts');
+    },
+    city: function() {
+      return Session.get('city');
+    }
+  });
+
+  Handlebars.registerHelper("prettifyDate", function(timestamp) {
+    var date = moment(timestamp);
+    return moment(date).format("MMM Do YY");
+});
+
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,39 @@
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+  	var appKey =Meteor.settings.gig_app_key;
+  	var cloudmadeKey = Meteor.settings.cloudmade_app_key;
+
+    Meteor.methods({
+	  	search5gig: function(city, country_iso){
+	        var request_uri = "http://www.nvivo.es/api/request.php?api_key="+appKey+"&method=city.getEvents&city="+city+"&country_iso="+country_iso+"&format=json";
+	        var result = HTTP.call("GET",request_uri);
+			if (result.statusCode === 200) {
+			  var respJson = JSON.parse(result.content);
+			  if(respJson.status==='error')
+			  	return respJson.error.message
+			  
+			  return respJson.response.gigs
+			}else {
+			  return "ERROR in the request"
+			}
+	  	},
+
+	  	getGeoCoordinates: function(city){
+	  		var request_uri = "http://beta.geocoding.cloudmade.com/v3/"+cloudmadeKey+"/api/geo.location.search.2?format=json&source=OSM&enc=UTF-8&limit=1&q="+city+"&token=96f4ae8a17f5435ca8adcc322e29058d";
+	  		console.log(request_uri);
+	  		var result = HTTP.call("GET",request_uri);
+
+	  		console.log(result);
+			if (result.statusCode === 200) {
+			  var respJson = JSON.parse(result.content);
+			  var place = respJson.places[0];
+			  return place.position
+			}else {
+			  return "No result"
+			}
+	  	}
+	  });
   });
+
+  
 }
